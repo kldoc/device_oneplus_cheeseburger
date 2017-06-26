b377cc9d5a571d593b4b32c8c5424e355c6e5e0e
Martinusbe <martinusbe@gmail.com>
2017-06-26 13:15:54 +0200

    tipsy
    Change-Id: I47d1d54aad828baa3bd55a6c252d6d6cb5fb28ce
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- tipsy.mk
+++ tipsy.mk
@@ -20,10 +20,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from cheeseburger device
 $(call inherit-product, device/oneplus/cheeseburger/device.mk)
 
-# Inherit some common XOS stuff
-$(call inherit-product, vendor/xos/config/common.mk)
+# Inherit some common Tipsy stuff.
+$(call inherit-product, vendor/tipsy/config/common_full_phone.mk)
 
-PRODUCT_NAME := XOS_cheeseburger
+PRODUCT_NAME := tipsy_cheeseburger
 PRODUCT_DEVICE := cheeseburger
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
@@ -33,17 +33,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 TARGET_VENDOR_PRODUCT_NAME := OnePlus5
 TARGET_VENDOR_DEVICE_NAME := OnePlus5
 PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=OnePlus5 PRODUCT_NAME=OnePlus5
-#PRODUCT_PROPERTY_OVERRIDES += \
-#    persist.ota.romname=$(PRODUCT_NAME) \
-#    persist.ota.version=$(shell date +%Y%m%d) \
-#    persist.ota.manifest=https://raw.githubusercontent.com/halogenOS/android_extras_ota/XOS-7.1/$(PRODUCT_NAME).xml
 
-PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.model
-
-#PRODUCT_BUILD_PROP_OVERRIDES += \
-#    BUILD_FINGERPRINT= \
-#    PRIVATE_BUILD_DESC=""
-#PRODUCT_PROPERTY_OVERRIDES += \
-#    ro.build.oemfingerprint=
+#PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.model
 
 TARGET_VENDOR := oneplus
--- vendorsetup.sh
+++
@@ -1,2 +0,0 @@
-add_lunch_combo XOS_cheeseburger-userdebug
-add_lunch_combo XOS_cheeseburger-eng
