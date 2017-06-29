0dcaae8a86abdd880db60dd00a947f3b1e6eb1c2
Martinusbe <martinusbe@gmail.com>
2017-06-29 09:37:58 +0200

    boardconfig: define msm8998 bootloader
    Change-Id: I3b11ca7c73a65d63b6370436a3e471801911b7a3
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,7 +42,10 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a9
 
-TARGET_NO_BOOTLOADER := false
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := msm8998
+TARGET_NO_BOOTLOADER := true
+
 TARGET_NO_KERNEL := false
 
 # Bluetooth
