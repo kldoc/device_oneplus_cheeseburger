85d5a6450d8dba0f39f4115f7c7b745d5936ef30
Martinusbe <martinusbe@gmail.com>
2017-06-30 13:43:09 +0200

    update BOARD_KERNEL_CMDLINE from oss
    Change-Id: I036b3ee576bbca9fde39e0f7d9028d82dbdd9ee9
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,16 +54,28 @@ QCOM_BT_USE_BTNV := true
 #QCOM_BT_USE_SMD_TTY := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := \
+TARGET_KERNEL_VERSION := 4.4
+ifeq ($(BOARD_KERNEL_CMDLINE),)
+ifeq ($(TARGET_KERNEL_VERSION),4.4)
+     BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc1b0000
+else
+     BOARD_KERNEL_CMDLINE += console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 earlycon=msm_hsl_uart,0xc1b0000
+endif
+BOARD_KERNEL_CMDLINE += \
         androidboot.hardware=qcom \
+        androidboot.configfs=true \
+        androidboot.usbcontroller=a800000.dwc3 \
         ehci-hcd.park=3 \
         sched_enable_hmp=1 \
         sched_enable_power_aware=1 \
         service_locator.enable=1 \
         swiotlb=2048 \
-        androidboot.configfs=true \
-        androidboot.usbcontroller=a800000.dwc3 \
+        user_debug=31 \
+        msm_rtb.filter=0x237 \
+        lpm_levels.sleep_disabled=1 \
+        swiotlb=2048 \
         androidboot.selinux=permissive
+endif
 
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
@@ -77,7 +89,6 @@ TARGET_KERNEL_SOURCE := kernel/oneplus/msm8998
 TARGET_KERNEL_CONFIG := oneplus5_defconfig
 TARGET_KERNEL_APPEND_DTB := true
 TARGET_COMPILE_WITH_MSM_KERNEL := true
-TARGET_KERNEL_VERSION := 4.4
 TARGET_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
 TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/rootdir/etc/fstab.qcom
